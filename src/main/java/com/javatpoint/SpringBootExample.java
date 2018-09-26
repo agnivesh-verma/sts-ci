a377a4ff04c2d50dc08728d37c7f351df31acb4c
Agnivesh Verma <agnivesh.verma@kelltontech.com>
2018-09-26 16:13:04 +0530

    First Commit

---
+++ src/main/java/com/javatpoint/SpringBootExample.java
@@ -0,0 +1,12 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			SpringApplication.run(SpringBootExample.class, args);
+	}
+
+}
