982407eae604265bfa1acc36b084b235027af947
KELLTON <KELLTON@KELLHYDLPTP045>
2018-10-03 13:11:34 +0530

    My second commit.

--- src/main/java/com/javatpoint/SpringBootExample.java
+++ src/main/java/com/javatpoint/SpringBootExample.java
@@ -6,6 +6,7 @@ public class SpringBootExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This is to run the Spring application - My first comment
 			SpringApplication.run(SpringBootExample.class, args);
 	}
 
